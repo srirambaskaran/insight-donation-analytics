06502dc228fbb915bbc8d976387fd384e23782f5
Sriram Baskaran <sbaskara@usc.edu>
2018-02-11 21:56:10 -0800

    Calculating percentile

--- insight-donation-analytics/src/org/insight/analytics/DataUtils.java
+++ insight-donation-analytics/src/org/insight/analytics/DataUtils.java
@@ -1,10 +1,20 @@
 package org.insight.analytics;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DataUtils {
-    public static int calculatePercentile(ArrayList<Double> values, double xthPercentile) {
-        
-        return 0;
+    
+    /**
+     * Percentile_index = (xthPercentile / 100 ) * size of list
+     * Percentile = values[Percentile_index]
+     * @param values
+     * @param xthPercentile
+     * @return 
+     */
+    public static double calculatePercentile(ArrayList<Double> values, double xthPercentile) {
+        Collections.sort(values);
+        int index = (int)Math.ceil( xthPercentile / 100 * values.size());
+        return values.get(index);
     }
 }

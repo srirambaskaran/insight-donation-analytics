5dd537945b314a73ea0047e5c2411adeb251acd4
Sriram Baskaran <sbaskara@usc.edu>
2018-02-12 19:48:12 -0800

    Updated percentile calculation method to standard nearest rank

--- insight-donation-analytics/src/org/insight/analytics/models/CommitteeDetails.java
+++ insight-donation-analytics/src/org/insight/analytics/models/CommitteeDetails.java
@@ -1,11 +1,16 @@
 package org.insight.analytics.models;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.PriorityQueue;
 
+/**
+ * This class stores the all details for a given committee, zipcode and year.
+ * It also calculates the requested percentile of the donations given.
+ * 
+ * @author srirambaskaran
+ *
+ */
 public class CommitteeDetails {
     private double runningTotal;
     private int size;
@@ -13,61 +18,53 @@ public class CommitteeDetails {
     private HashSet<Donor> addedDonors;
     private HashMap<Donor, HashMap<Integer, ArrayList<Double>>> allDonations;
     
-    private PriorityQueue<Double> firstK; //holds the first k values in the (committee+year) combo.
-    private PriorityQueue<Double> remaining;
+    private ArrayList<Double> values;
     
     public CommitteeDetails(HashMap<Donor, HashMap<Integer, ArrayList<Double>>> allDonations) {
         super();
         this.runningTotal = 0.0;
         this.addedDonors = new HashSet<>();
         this.size = 0;
-        this.firstK = new PriorityQueue<>(Comparator.reverseOrder());
-        this.remaining = new PriorityQueue<>();
-        this.allDonations = allDonations; //stores only reference from StremAnalyzer class.
+        this.allDonations = allDonations; //stores only reference from StreamAnalyzer class.
+        this.values = new ArrayList<>();
     }
     
     public double getRunningTotal() {
         return runningTotal;
     }
     
-    public void updateQueues(double amount, int k) {
-        if(!firstK.isEmpty() && firstK.peek() > amount) {
-            firstK.add(amount);
-            while(firstK.size() > k)
-                remaining.add(firstK.poll());
-        }else {
-            remaining.add(amount);
-            while(firstK.size() < k)
-                firstK.add(remaining.poll());
-        }
-    }
-    
-    public void addAmount(double amount, Donor donor, int year, double xthPercentile) {
+    /**
+     * This method finds all donations in the given year for a recently identified
+     * as repeated donor and adds to the transactions of (committee+zipcode+year).
+     *  
+     * @param amount
+     * @param donor
+     * @param year
+     */
+        
+    public void addAmount(double amount, Donor donor, int year) {
         if (!addedDonors.contains(donor)) {
             ArrayList<Double> donations = allDonations.get(donor).get(year);
             for(Double donation: donations)
-                addAmount(donation, xthPercentile);
+                addAmount(donation);
             addedDonors.add(donor);
         } else {
-            addAmount(amount, xthPercentile);
+            addAmount(amount);
         }
     }
     
-    public void addAmount(double amount, double xthPercentile) {
+    
+    public void addAmount(double amount) {
         this.size ++;
-        int index = calculatePercentileIndex(xthPercentile);
         this.runningTotal += amount;
-        updateQueues(amount, index);
+        this.values.add(amount);
     }
     
-    public int calculatePercentileIndex(double xthPercentile) {
-        return (int)Math.ceil( xthPercentile / 100 * this.size);
+    public double getPercentile(double xthPercentile) {
+        int index = (int)(Math.ceil( xthPercentile / 100 * this.size));
+        return this.values.get(index - 1);
     }
     
-    public double getPercentile() {
-        return this.firstK.peek();
-    }
-
     public int size() {
         return this.size;
     }

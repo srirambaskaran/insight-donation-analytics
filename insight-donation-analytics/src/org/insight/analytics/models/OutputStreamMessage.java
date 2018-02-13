71c001c252e42ee85672c713de79da5e3bafd9d6
Sriram Baskaran <sbaskara@usc.edu>
2018-02-12 19:48:41 -0800

    Minor style fixes

--- insight-donation-analytics/src/org/insight/analytics/models/OutputStreamMessage.java
+++ insight-donation-analytics/src/org/insight/analytics/models/OutputStreamMessage.java
@@ -1,12 +1,12 @@
 package org.insight.analytics.models;
 
 public class OutputStreamMessage {
-    
+
     private Committee committee;
     private double total;
     private double percentile;
     private int numberRepeatDonors;
-    
+
     public OutputStreamMessage(Committee committee, double total, double percentile, int numberRepeatDonors) {
         super();
         this.committee = committee;
@@ -14,21 +14,25 @@ public class OutputStreamMessage {
         this.percentile = percentile;
         this.numberRepeatDonors = numberRepeatDonors;
     }
-    
+
     public Committee getCommittee() {
         return committee;
     }
+
     public double getTotal() {
         return total;
     }
+
     public double getPercentile() {
         return percentile;
     }
+
     public int getNumberRepeatDonors() {
         return numberRepeatDonors;
     }
-    
+
     public String toString() {
-        return committee.getCommitteeId()+"|"+committee.getZipCode()+"|"+committee.getYear()+"|"+Math.round(percentile)+"|"+Math.round(total)+"|"+numberRepeatDonors;
+        return committee.getCommitteeId() + "|" + committee.getZipCode() + "|" + committee.getYear() + "|"
+                + Math.round(percentile) + "|" + Math.round(total) + "|" + numberRepeatDonors;
     }
 }
